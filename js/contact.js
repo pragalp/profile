5bdd458cdb5beec103be358e47a73b7aa955ccb2
pragalp <pragalp335.com>
2025-12-05 19:20:19 +0530

    yes

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,13 @@
+emailjs.init("ebnziiJkiOu7-iK4G");
+
 const contactForm = document.getElementById('contact-form');
 const contactMessage = document.getElementById('contact-message');
 
 const sendEmail = (e) => {
     e.preventDefault();
 
-
     emailjs.sendForm('service_zlaejj5', 'template_13too8j', '#contact-form', 'ebnziiJkiOu7-iK4G')
         .then(() => {
-            // Success message
             contactMessage.textContent = '✅ Message sent successfully!';
             contactMessage.className = 'contact-message show success';
 
@@ -17,18 +17,15 @@ const sendEmail = (e) => {
             }, 5000);
 
             contactForm.reset();
-        },
-            () => {
-                // Error message
-                contactMessage.textContent = '❌ Message not sent (service error)';
-                contactMessage.className = 'contact-message show error';
+        }, () => {
+            contactMessage.textContent = '❌ Message not sent (service error)';
+            contactMessage.className = 'contact-message show error';
 
-                setTimeout(() => {
-                    contactMessage.className = 'contact-message';
-                    contactMessage.textContent = '';
-                }, 5000);
-            }
-        );
+            setTimeout(() => {
+                contactMessage.className = 'contact-message';
+                contactMessage.textContent = '';
+            }, 5000);
+        });
 };
 
 contactForm.addEventListener('submit', sendEmail);
